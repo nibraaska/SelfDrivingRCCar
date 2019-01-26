d0f37758bf0ce591ccbb50a01299c786d787cadb
spies36 <dylan.spies36@gmail.com>
2019-01-25 23:56:14 -0600

    Fixed the car.py values

--- car.py
+++ car.py
@@ -6,8 +6,8 @@ from time import sleep
 PWM_FORWARD_LEFT_PIN = 26	# IN1 - Forward Drive
 PWM_REVERSE_LEFT_PIN = 19	# IN2 - Reverse Drive
 # Motor B, Right Side GPIO CONSTANTS
-PWM_FORWARD_RIGHT_PIN = 13	# IN1 - Forward Drive
-PWM_REVERSE_RIGHT_PIN = 6	# IN2 - Reverse Drive
+PWM_FORWARD_RIGHT_PIN = 13	# IN1 - Left Drive
+PWM_REVERSE_RIGHT_PIN = 6	# IN2 - Right Drive
  
 # Initialise objects for H-Bridge PWM pins
 # Set initial duty cycle to 0 and frequency to 1000
@@ -25,70 +25,30 @@ def allStop():
 	reverseRight.value = 0
  
 def forwardDrive():
-	forwardLeft.value = 1.0
+	forwardLeft.value = 0.25
 	reverseLeft.value = 0
-	forwardRight.value = 1.0
+	forwardRight.value = 0.0
 	reverseRight.value = 0
- 
-def reverseDrive():
-	forwardLeft.value = 0
-	reverseLeft.value = 1.0
-	forwardRight.value = 0
-	reverseRight.value = 1.0
- 
-def spinLeft():
-	forwardLeft.value = 0
-	reverseLeft.value = 1.0
-	forwardRight.value = 1.0
-	reverseRight.value = 0
- 
-def SpinRight():
-	forwardLeft.value = 1.0
-	reverseLeft.value = 0
-	forwardRight.value = 0
-	reverseRight.value = 1.0
  
 def forwardTurnLeft():
 	forwardLeft.value = 0.2
 	reverseLeft.value = 0
-	forwardRight.value = 0.8
+	forwardRight.value = 1.0
 	reverseRight.value = 0
  
 def forwardTurnRight():
-	forwardLeft.value = 0.8
+	forwardLeft.value = 0.2
 	reverseLeft.value = 0
-	forwardRight.value = 0.2
-	reverseRight.value = 0
- 
-def reverseTurnLeft():
-	forwardLeft.value = 0
-	reverseLeft.value = 0.2
 	forwardRight.value = 0
-	reverseRight.value = 0.8
- 
-def reverseTurnRight():
-	forwardLeft.value = 0
-	reverseLeft.value = 0.8
-	forwardRight.value = 0
-	reverseRight.value = 0.2
+	reverseRight.value = 1.0
  
 def main():
 	allStop()
 	forwardDrive()
 	sleep(5)
-	reverseDrive()
-	sleep(5)
-	spinLeft()
-	sleep(5)
-	SpinRight()
-	sleep(5)
 	forwardTurnLeft()
 	sleep(5)
 	forwardTurnRight()
-	sleep(5)
-	reverseTurnLeft()
-	sleep(5)
-	reverseTurnRight()
 	sleep(5)
 	allStop()
  
