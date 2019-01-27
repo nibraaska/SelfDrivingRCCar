4c009ba6333cb3ca623408d6dae8f850fbc30442
spies36 <dylan.spies36@gmail.com>
2019-01-27 03:09:45 -0600

    Records video as stills and puts labels in file

--- python/camera.py
+++ python/camera.py
@@ -3,15 +3,8 @@ import picamera
 import numpy as np
 from time import sleep
 
-def pic():
+def pic(x):
 	with picamera.PiCamera() as camera:
-		camera.resolution = (64, 64)
-		camera.framerate = 24
-		output = np.empty((64,64,3), dtype=np.uint8)
-		camera.capture(output, 'rgb')
-		arr = np.dot(output[...,:3], [0.299, 0.587, 0.114])
-		arr = (arr / 255.0) + 0.01
-		np.savetxt('test.txt', arr.reshape(1, 4096), delimiter=',', fmt='%1g')
-f = open('test.txt', 'w')
-pic()
+		camera.resolution = (128, 128)
+		camera.capture('/home/pi/Pictures/image' + str(x) + '.png')
 
--- python/key_con.py
+++ python/key_con.py
@@ -1,25 +1,38 @@
 import car
+import camera
 from time import sleep
 import sys
 import tkinter as tk
 
+x = 0
+f = open("/home/pi/Documents/decisions.txt", 'w+')
 def key_input(event):
+	global x
+	global f
 	print('Key:', event.char)
 	key_press = event.char
 	sleep_time = 0.030
 	if(key_press.lower() =='w'):
-		car.move([1,0,0,0])
-		sleep(0.03)
+		#car.move([1,0,0,0])
+		sleep(sleep_time)
+		camera.pic(x)
+		f.write("1000\n")
 		command.bind('<KeyRelease-w>', car.allStop())
 	elif(key_press.lower() == 'a'):
-		car.move([1,0,1,0])
-		sleep(1)
+		#car.move([1,0,1,0])
+		sleep(sleep_time)
+		camera.pic(x)
+		f.write("1010\n")
 		command.bind('<KeyRelease-a>', car.allStop())
 	elif(key_press.lower() == 'd'):
-		car.move([1,0,0,1])
-		sleep(1)
+		#car.move([1,0,0,1])
+		sleep(sleep_time)
+		camera.pic(x)
+		f.write("1001\n")
 		command.bind('<KeyRelease-d>', car.allStop())
+	x += 1
 
 command = tk.Tk()
 command.bind('<KeyPress>', key_input)
 command.mainloop()
+f.close()
